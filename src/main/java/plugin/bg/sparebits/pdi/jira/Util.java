54f6ade8d1bf2647fbc2261224ed19476cd91f74
Matt Tucker <matt.tucker@pentaho.com>
2016-05-16 09:58:03 -0400

    Replace ValueMeta with ValueMetaFactory
    ValueMeta is deprecated in newer PDI versions

--- src/main/java/plugin/bg/sparebits/pdi/jira/Util.java
+++ src/main/java/plugin/bg/sparebits/pdi/jira/Util.java
@@ -1,7 +1,19 @@
-/*
- * Util.java
- * Created on 15.10.2013 14:22:02 
- */
+/*! ***************************************************************************
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ ******************************************************************************/
+
 package plugin.bg.sparebits.pdi.jira;
 
 import java.util.ArrayList;
@@ -9,9 +21,10 @@ import java.util.List;
 import java.util.Map;
 import java.util.TreeMap;
 
+import org.pentaho.di.core.exception.KettlePluginException;
 import org.pentaho.di.core.row.RowMetaInterface;
-import org.pentaho.di.core.row.ValueMeta;
 import org.pentaho.di.core.row.ValueMetaInterface;
+import org.pentaho.di.core.row.value.ValueMetaFactory;
 
 
 /**
@@ -31,7 +44,6 @@ public class Util {
     public static Map<String, ValueMetaInterface> getFields(String prefix, Map<String, Object> json) {
         Map<String, ValueMetaInterface> fields = new TreeMap<String, ValueMetaInterface>();
         for (String key : json.keySet()) {
-            ValueMetaInterface field = new ValueMeta(prefix + key);
             Object value = json.get(key);
             if (value instanceof ArrayList) {
                 if (!((List) value).isEmpty() && ((List) value).get(0) instanceof Map) {
@@ -42,7 +54,12 @@ public class Util {
                     fields.putAll(getFields(prefix + key + "_", (Map<String, Object>) value));
                 }
             } else {
-                field.setType(getType(value));
+                ValueMetaInterface field;
+                try {
+                    field = ValueMetaFactory.createValueMeta(prefix + key, getType(value));
+                } catch ( KettlePluginException e ) {
+                    throw new RuntimeException( e );
+                }
                 fields.put(prefix + key, field);
             }
         }
